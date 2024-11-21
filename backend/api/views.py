ba1af991bc51b084aff5aadd77bd005419f4fd5d
j1ere <jeremywambua106@gmail.com>
2024-11-21 13:44:42 +0300

    sending json data to view and sending it from the view back to client as a python dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,16 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+import json
 from django.http import JsonResponse
 
 # Create your views here.
+@csrf_exempt
 def api_view(request, *args, **kwargs):
-    return JsonResponse({"message": "I love you django rest"})
+    from_basic=request.body
+    data = {}
+    try:
+        data = json.loads(from_basic) #converting JSON data to python dictionary
+    except:
+        pass
+    print(data.keys())
+    return JsonResponse(data)
